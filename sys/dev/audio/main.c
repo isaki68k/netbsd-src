6b95d515e9059979ed36d53775ed321408f78bd7
Tetsuya Isaki <isaki@pastel-flower.jp>
2017-10-26 12:06:57 +0900

    WAV_API_TEST パートを削除。もういらない。

--- sys/dev/audio/main.c
+++ sys/dev/audio/main.c
@@ -4,7 +4,6 @@
 #endif
 
 //#define PSG_TEST
-//#define WAV_API_TEST
 #define ADPCM_TEST
 
 #ifdef ADPCM_TEST
@@ -589,31 +588,5 @@ main(void)
 	printf("END\n");
 	getchar();
 	return 0;
-}
-#endif
-
-#ifdef WAV_API_TEST
-
-#include <windows.h>
-#include <mmsystem.h>
-#pragma comment(lib, "winmm.lib")
-
-int main()
-{
-	HWAVEOUT hWaveOut = 0;
-	WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
-	waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
-	char buffer[8000 * 60] = { 0, };
-
-	// See http://goo.gl/hQdTi
-	for (DWORD t = 0; t < sizeof(buffer); ++t)
-		buffer[t] = (char)((((t * (t >> 8 | t >> 9) & 46 & t >> 8)) ^ (t & t >> 13 | t >> 6)) & 0xFF);
-
-	WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
-	waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
-	waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
-	waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
-	waveOutClose(hWaveOut);
-	Sleep(60 * 1000);
 }
 #endif
