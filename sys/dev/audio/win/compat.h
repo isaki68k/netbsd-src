843c112bb0a24897f3c49e24d581ab3a928bfad6
moveccr <yosuke@sss.rxz.jp>
2017-10-26 14:34:43 +0900

    win compat.h にエンディアン変換の互換関数を実装。

--- sys/dev/audio/win/compat.h
+++ sys/dev/audio/win/compat.h
@@ -23,6 +23,46 @@ uint32_t __builtin_bswap32(uint32_t a)
 	return (a << 24) | ((a << 8) & 0x00ff0000) | ((a >> 8) & 0x0000ff00) | (a >> 24);
 }
 
+inline
+uint16_t le16toh(uint16_t a)
+{
+#if BYTE_ORDER == LITTLE_ENDIAN
+	return a;
+#else
+	return __builtin_bswap16(a);
+#endif
+}
+
+inline
+uint16_t be16toh(uint16_t a)
+{
+#if BYTE_ORDER == BIG_ENDIAN
+	return a;
+#else
+	return __builtin_bswap16(a);
+#endif
+}
+
+inline
+uint32_t le32toh(uint32_t a)
+{
+#if BYTE_ORDER == LITTLE_ENDIAN
+	return a;
+#else
+	return __builtin_bswap32(a);
+#endif
+}
+
+inline
+uint32_t be32toh(uint32_t a)
+{
+#if BYTE_ORDER == BIG_ENDIAN
+	return a;
+#else
+	return __builtin_bswap32(a);
+#endif
+}
+
 inline void
 panic(const char *fmt, ...)
 {
