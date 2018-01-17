6c12f30fc78fee563bb21230f79527bf457dca6e
Tetsuya Isaki <isaki@pastel-flower.jp>
2018-01-17 17:55:16 +0900

    NetBSD 風に整形中。

--- sys/dev/audio/audiovar.h
+++ sys/dev/audio/audiovar.h
@@ -1,3 +1,4 @@
+// vi:set ts=8:
 #pragma once
 
 // デバッグレベルは
@@ -81,7 +82,7 @@
 #endif
 
 
-/* 前方参照 */
+// 前方参照
 typedef struct audio_ring audio_ring_t;
 typedef struct audio_track audio_track_t;
 typedef struct audio_trackmixer audio_trackmixer_t;
@@ -89,25 +90,24 @@ typedef struct audio_file audio_file_t;
 typedef struct audio_convert_arg audio_convert_arg_t;
 
 #if defined(FREQ_ORIG)
-/*
-* 簡易帯分数表現
-*/
+// 簡易帯分数表現
 typedef struct audio_rational {
 	int i;
 	int n;
 } audio_rational_t;
 #endif
 
-// リングバッファ
+/* ring buffer */
 struct audio_ring
 {
-	audio_format2_t fmt;	/* フォーマット */
-	int  capacity;			/* 容量(フレーム) */
-	int  top;				/* 先頭フレーム位置 */
-	int  count;				/* 有効フレーム量 */
-	void *sample;			/* サンプル */
+	audio_format2_t fmt;	/* format */
+	int  capacity;		/* capacity by frame */
+	int  top;		/* top frame position */
+	int  count;		/* available frame count */
+	void *sample;		/* sample ptr */
 };
 
+/* conversion stage */
 struct audio_stage
 {
 	audio_filter_t filter;
@@ -118,9 +118,9 @@ struct audio_stage
 typedef struct audio_stage audio_stage_t;
 
 typedef enum {
-	AUDIO_STATE_CLEAR,		// データなし (drain不要)
-	AUDIO_STATE_RUNNING,		// データあり
-	AUDIO_STATE_DRAINING,		// drain 中
+	AUDIO_STATE_CLEAR,	/* no data, no need to drain */
+	AUDIO_STATE_RUNNING,	/* need to drain */
+	AUDIO_STATE_DRAINING,	/* now draining */
 } audio_state_t;
 
 struct audio_track
@@ -133,123 +133,133 @@ struct audio_track
 	// file->mode は録再トラックの mode を OR したものと一致しているはず。
 	int mode;
 
-	audio_ring_t        usrbuf;			// ユーザ入出力バッファ
-	u_int               usrbuf_nblks;	// usrbuf のブロック数
-	u_int               usrbuf_blksize;	// usrbuf のブロックサイズ
+	audio_ring_t	usrbuf;		/* user i/o buffer */
+	u_int		usrbuf_nblks;	/* block num of usrbuf */
+	u_int		usrbuf_blksize;	/* block size of usrbuf */
 
-	audio_format2_t     inputfmt;		// このトラックに入力するフォーマット
-	audio_ring_t       *input;			// このトラックに入力するとき使用するバッファへのポインタ
+	audio_format2_t	inputfmt;	/* track input format. */
+					/* userfmt for play, mixerfmt for rec */
+	audio_ring_t	*input;		/* ptr to input stage buffer */
 
-	audio_ring_t        outputbuf;		/* トラックの出力バッファ */
-	kcondvar_t          outchan;		// 出力バッファが空いたことの通知用
+	audio_ring_t	outputbuf;	/* track output buffer */
+	kcondvar_t	outchan;	// I/O ready になったことの通知用
 
-	audio_stage_t       codec;			// エンコーディング変換ステージ
-	audio_stage_t       chvol;			// チャンネルボリュームステージ
-	audio_stage_t       chmix;			// チャンネルミックスステージ
-	audio_stage_t       freq;			// 周波数変換ステージ
+	audio_stage_t	codec;		/* encoding conversion stage */
+	audio_stage_t	chvol;		/* channel volume stage */
+	audio_stage_t	chmix;		/* channel mix stage */
+	audio_stage_t	freq;		/* frequency conversion stage */
 
 #if defined(FREQ_CYCLE2)
-	unsigned int        freq_step;		/* 周波数変換用、周期比 */
-	unsigned int        freq_current;	/* 周波数変換用、現在のカウンタ */
-	unsigned int        freq_leap;		/* 周波数変換用、補正値 */
+	u_int		freq_step;	// 周波数変換用、周期比
+	u_int		freq_current;	// 周波数変換用、現在のカウンタ
+	u_int		freq_leap;	// 周波数変換用、補正値
 #elif defined(FREQ_ORIG)
-	audio_rational_t   freq_step;		/* 周波数変換用分数 (変換元周波数 / 変換先周波数) */
-	audio_rational_t   freq_current;	/* 周波数変換用 現在のカウンタ */
-	int32_t            freq_coef;		// 周波数変換用係数
+	audio_rational_t freq_step;	// 周波数変換用分数 (srcfreq/dstfreq)
+	audio_rational_t freq_current;	// 周波数変換用 現在のカウンタ
+	int32_t		freq_coef;	// 周波数変換用係数
 #else
 #error unknown FREQ_*
 #endif
 
-	uint16_t  ch_volume[AUDIO_MAX_CHANNELS];	/* チャンネルバランス用 チャンネルボリューム */
-	uint               volume;			// トラックボリューム (内部表現 0..256)
+	uint16_t ch_volume[AUDIO_MAX_CHANNELS];	/* channel volume(0..256) */
+	u_int		volume;		/* track volume (0..256) */
 
-	audio_trackmixer_t  *mixer;			/* 接続されているトラックミキサ */
+	audio_trackmixer_t *mixer;	/* connected track mixer */
 
-	uint64_t seq;	// トラックミキサが引き取ったシーケンス番号
+	// トラックミキサが引き取ったシーケンス番号
+	uint64_t	seq;		/* seq# picked up by track mixer */
 
-	audio_state_t pstate;				// 再生のステート
-	bool is_pause;						/* outputbuf への trackmixer からのアクセスを一時停止する */
+	audio_state_t	pstate;		/* playback state */
+	bool		is_pause;
 
-	void *sih_wr;						/* softint cookie for write */
+	void		*sih_wr;	/* softint cookie for write */
 
-	uint64_t inputcounter;				/* トラックに入力されたフレーム数 */
-	uint64_t outputcounter;				/* トラックから出力されたフレーム数 */
-	uint64_t inputbytes;				// ユーザランドから入力されたバイト数
-	uint64_t outputbytes;				// ユーザランドに出力したバイト数
+	uint64_t	inputcounter;	/* トラックに入力されたフレーム数 */
+	uint64_t	outputcounter;	/* トラックから出力されたフレーム数 */
+	uint64_t	inputbytes;	// ユーザランドから入力されたバイト数
+	uint64_t	outputbytes;	// ユーザランドに出力したバイト数
 
 #if !defined(AUDIO_SOFTINTR)
-	volatile uint32_t track_cl;			// track cooperative lock
+	volatile uint32_t track_cl;	/* track cooperative lock */
 #endif
 
-	/* できるかどうか未知 */
-	uint64_t track_mixer_counter;		/* outputbuf のトラックミキサ側読み書きフレーム数 */
-	uint64_t mixer_hw_counter; /* mixer <-> hw 入出力フレーム数 */
-	uint64_t hw_complete_counter; /* ハードウェア I/O が完了したフレーム数 */
+	// 以下できるかどうか未知
 
-	/* デバッグ用 */
-	int id;								/* トラック識別子 */
+	// outputbuf のトラックミキサ側読み書きフレーム数
+	uint64_t	track_mixer_counter;
+	// mixer <-> hw 入出力フレーム数
+	uint64_t	mixer_hw_counter;
+	// ハードウェア I/O が完了したフレーム数
+	uint64_t	hw_complete_counter;
+
+	int		id;		/* track id for debug */
 };
 
 struct audio_trackmixer
 {
-	int mode;							/* AUMODE_PLAY or AUMODE_RECORD */
-	audio_format2_t track_fmt;			/* track <-> trackmixer フォーマット */
+	int		mode;		/* AUMODE_PLAY or AUMODE_RECORD */
+	audio_format2_t	track_fmt;	/* track <-> trackmixer format */
 
-	int frames_per_block;				/* 内部周波数での 1 ブロックのフレーム数 */
+	int		frames_per_block; /* number of frames in 1 block */
 
-	uint           volume;				/* 出力マスタボリューム */
+	u_int		volume;		/* output SW master volume (0.256) */
 
-	audio_format2_t mixfmt;				// PLAY 合成用整数倍精度フォーマット
-	void *mixsample;					// PLAY 合成用整数倍精度バッファ
+	audio_format2_t	mixfmt;
+	void		*mixsample;	/* wide-int-sized mixing buf */
 
-	audio_filter_t  codec;				// MD が要求する追加のコーデック
-	audio_filter_arg_t codecarg;		// その引数
-	audio_ring_t    codecbuf;			// コーデック用バッファ。ストライド変換の吸収
+	audio_filter_t	codec;		/* MD codec */
+	audio_filter_arg_t codecarg;	/* and its argument */
+	audio_ring_t	codecbuf;	/* also used for wide->int conversion */
 
-	audio_ring_t   hwbuf;				/* 物理デバイスの入出力バッファ (malloc ではなく allocm で確保する) */
-	int hwblks;							// hwbuf のブロック数
-	kcondvar_t     intrcv;				/* 割り込みを通知する? */
+	audio_ring_t	hwbuf;		/* HW I/O buf */
+	int		hwblks;		/* number of blocks in hwbuf */
+	kcondvar_t	intrcv;		// 割り込みを通知する?
 
-	uint64_t mixseq;	// ミキシング中のシーケンス番号
-	uint64_t hwseq;		// ハードウェア出力完了したシーケンス番号
+	uint64_t	mixseq;		/* seq# currently being mixed */
+	uint64_t	hwseq;		/* seq# HW output completed */
+				// ハードウェア出力完了したシーケンス番号
 
-	struct audio_softc *sc;				/* 論理デバイス */
+	struct audio_softc *sc;
 
-	int blktime_n; // ブロックの秒の分子 初期値は AUDIO_BLK_MS
-	int blktime_d; // ブロックの秒の分母 初期値は 1000
+	/* initial blktime n/d = AUDIO_BLK_MS / 1000 */
+	int		blktime_n;	/* blk time numerator */
+	int		blktime_d;	/* blk time denominator */
 
 #if defined(AUDIO_SOFTINTR)
-	kmutex_t softintrlock;				// softintr mutex
-	void *softintr;						// softintr cookie
+	kmutex_t	softintrlock;	/* softintr mutex */
+	void		*softintr;	/* softintr cookie */
 #endif
-										// 未定
 
-	uint64_t hw_output_counter;			/* ハードウェアへの出力フレーム数 */
-	uint64_t hw_complete_counter;		/* ハードウェアが出力完了したフレーム数 */
+	// 以下未定
+
+	// ハードウェアへの出力フレーム数
+	uint64_t	hw_output_counter;
+	// ハードウェアが出力完了したフレーム数
+	uint64_t	hw_complete_counter;
 };
 
 struct audio_file
 {
-	struct audio_softc *sc;			/* 論理デバイス */
+	struct audio_softc *sc;
 
 	// ptrack, rtrack はトラックが無効なら NULL。
 	// mode に AUMODE_{PLAY,RECORD} が立っていても該当側の ptrack, rtrack
 	// が NULL という状態はありえる。例えば、上位から PLAY を指定されたが
 	// 何らかの理由で ptrack が有効に出来なかった、あるいはクローズに
 	// 向かっている最中ですでにこのトラックが解放された、とか。
-	audio_track_t   *ptrack;		/* 再生トラック */
-	audio_track_t   *rtrack;		/* 録音トラック */
+	audio_track_t	*ptrack;	/* play track (if available) */
+	audio_track_t	*rtrack;	/* record track (if available) */
 
 	// この file の再生/録音モード。AUMODE_* (PLAY_ALL も含む)
 	// ptrack.mode は (mode & (AUMODE_PLAY | AUMODE_PLAY_ALL)) と、
 	// rtrack.mode は (mode & AUMODE_RECORD) と等しいはず。
-	int mode;
-	dev_t dev;						/* デバイスファイルへのバックリンク */
+	int		mode;
+	dev_t		dev;		// デバイスファイルへのバックリンク
 
-	pid_t async_audio;				/* process who wants audio SIGIO */
+	pid_t		async_audio;	/* process who wants audio SIGIO */
 
 #if defined(USE_SETCHAN)
-	int ioctl_target;				// ioctl 発行先トラック番号
+	int ioctl_target;		// ioctl 発行先トラック番号
 #endif
 
 	SLIST_ENTRY(audio_file) entry;
