0a2e90199734113dfc20e15738e725f501684ffa
Tetsuya Isaki <isaki@pastel-flower.jp>
2018-04-12 21:24:12 +0900

    style. include guard, extern.

--- sys/dev/audio/aucodec.h
+++ sys/dev/audio/aucodec.h
@@ -1,10 +1,13 @@
-#pragma once
+#ifndef _SYS_DEV_AUDIO_AUCODEC_H_
+#define _SYS_DEV_AUDIO_AUCODEC_H_
 
-void audio_internal_to_linear8(audio_filter_arg_t *);
-void audio_internal_to_linear16(audio_filter_arg_t *);
-void audio_internal_to_linear24(audio_filter_arg_t *);
-void audio_internal_to_linear32(audio_filter_arg_t *);
-void audio_linear8_to_internal(audio_filter_arg_t *);
-void audio_linear16_to_internal(audio_filter_arg_t *);
-void audio_linear24_to_internal(audio_filter_arg_t *);
-void audio_linear32_to_internal(audio_filter_arg_t *);
+extern void audio_internal_to_linear8(audio_filter_arg_t *);
+extern void audio_internal_to_linear16(audio_filter_arg_t *);
+extern void audio_internal_to_linear24(audio_filter_arg_t *);
+extern void audio_internal_to_linear32(audio_filter_arg_t *);
+extern void audio_linear8_to_internal(audio_filter_arg_t *);
+extern void audio_linear16_to_internal(audio_filter_arg_t *);
+extern void audio_linear24_to_internal(audio_filter_arg_t *);
+extern void audio_linear32_to_internal(audio_filter_arg_t *);
+
+#endif /* !_SYS_DEV_AUDIO_AUCODEC_H_ */
