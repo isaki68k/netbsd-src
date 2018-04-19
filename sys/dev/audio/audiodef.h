c197851d57a12b31d79e9b5e9e43a12a30c2b0e6
Tetsuya Isaki <isaki@pastel-flower.jp>
2018-04-19 16:46:16 +0900

    auring.h を audiodef.h にマージする。有効範囲は同じなので。

--- sys/dev/audio/audiodef.h
+++ sys/dev/audio/audiodef.h
@@ -38,6 +38,17 @@
 // C の実装定義動作を使用する。
 #define AUDIO_USE_C_IMPLEMENTATION_DEFINED_BEHAVIOR
 
+/*
+ * Audio Ring Buffer.
+ */
+
+#ifdef DIAGNOSTIC
+#define DIAGNOSTIC_ring(ring)	audio_diagnostic_ring(__func__, (ring))
+extern void audio_diagnostic_ring(const char *, const audio_ring_t *);
+#else
+#define DIAGNOSTIC_ring(ring)
+#endif
+
 static inline int
 frametobyte(const audio_format2_t *fmt, int frames)
 {
@@ -53,10 +64,176 @@ frame_per_block(const audio_trackmixer_t *mixer, const audio_format2_t *fmt)
 	    mixer->blktime_d;
 }
 
-#if defined(_KERNEL)
-#include <dev/audio/auring.h>
-#else
-#include "auring.h"
-#endif
+// idx をラウンディングします。
+// 加算方向で、加算量が ring->capacity 以下のケースのみサポートします。
+/*
+ * Round idx.  idx must be non-negative and less than 2 * capacity.
+ * This is a private function.
+ */
+static inline int
+audio_ring_round(const audio_ring_t *ring, int idx)
+{
+	DIAGNOSTIC_ring(ring);
+	KASSERT(idx >= 0);
+	KASSERT(idx < ring->capacity * 2);
+
+	if (idx < ring->capacity) {
+		return idx;
+	} else {
+		return idx - ring->capacity;
+	}
+}
+
+// ring の tail 位置(head+used位置) を返します。
+// この位置は、最終有効フレームの次のフレーム位置に相当します。
+/*
+ * Return ring's tail (= head + used) position.
+ */
+static inline int
+audio_ring_tail(const audio_ring_t *ring)
+{
+	return audio_ring_round(ring, ring->head + ring->used);
+}
+
+// ring の head フレームのポインタを求めます。
+/*
+ * Return ring's head pointer.
+ * This function can be used only if the stride of the 'ring' is equal to
+ * the internal stride.  Don't use this for hw buffer.
+ */
+static inline aint_t *
+audio_ring_headptr_aint(const audio_ring_t *ring)
+{
+	KASSERT(ring->fmt.stride == sizeof(aint_t) * NBBY);
+
+	return (aint_t *)ring->mem + ring->head * ring->fmt.channels;
+}
+
+// ring の tail (= head + used、すなわち、最終有効フレームの次) フレームの
+// ポインタを求めます。
+// hwbuf のポインタはこちらではなく RING_BOT_UINT8() で取得してください。
+/*
+ * Return ring's tail (= head + used) pointer.
+ * This function can be used only if the stride of the 'ring' is equal to
+ * the internal stride.  Don't use this for hw buffer.
+ */
+static inline aint_t *
+audio_ring_tailptr_aint(const audio_ring_t *ring)
+{
+	KASSERT(ring->fmt.stride == sizeof(aint_t) * NBBY);
+
+	return (aint_t *)ring->mem + audio_ring_tail(ring) * ring->fmt.channels;
+}
+
+// ring の head フレームのポインタを求めます。
+/*
+ * Return ring's head pointer.
+ * This function can be used even if the stride of the 'ring' is equal to
+ * or not equal to the internal stride.
+ */
+static inline uint8_t *
+audio_ring_headptr(const audio_ring_t *ring)
+{
+	return (uint8_t *)ring->mem +
+	    ring->head * ring->fmt.channels * ring->fmt.stride / NBBY;
+}
+
+// ring の tail (= head + used、すなわち、最終有効フレームの次) フレームの
+// ポインタを求めます。HWbuf は 4bit/sample の可能性があるため RING_BOT() では
+// なく必ずこちらを使用してください。
+/*
+ * Return ring's tail pointer.
+ * This function can be used even if the stride of the 'ring' is equal to
+ * or not equal to the internal stride.
+ */
+static inline uint8_t *
+audio_ring_tailptr(audio_ring_t *ring)
+{
+	return (uint8_t *)ring->mem +
+	    audio_ring_tail(ring) * ring->fmt.channels * ring->fmt.stride / NBBY;
+}
+
+// キャパシティをバイト単位で求めます。
+/*
+ * Return ring's capacity in bytes.
+ */
+static inline int
+audio_ring_bytelen(const audio_ring_t *ring)
+{
+	// return frametobyte(ring, ring->capacity)
+	return ring->capacity * ring->fmt.channels * ring->fmt.stride / NBBY;
+}
+
+// ring->head から n 個取り出したことにします。
+/*
+ * Take out n frames from head of ring.
+ * This function only manipurates counters.  It doesn't manipurate any
+ * actual buffer data.
+ */
+static inline void
+audio_ring_take(audio_ring_t *ring, int n)
+{
+	DIAGNOSTIC_ring(ring);
+	KASSERTMSG(n >= 0, "%s: n=%d", __func__, n);
+	KASSERTMSG(ring->used >= n, "%s: ring->used=%d n=%d",
+	    __func__, ring->used, n);
+
+	ring->head = audio_ring_round(ring, ring->head + n);
+	ring->used -= n;
+}
+
+// ring tail に n 個付け足したことにします。
+/*
+ * Append n frames into tail of ring.
+ * This function only manipurates counters.  It doesn't manipurate any
+ * actual buffer data.
+ */
+static inline void
+audio_ring_push(audio_ring_t *ring, int n)
+{
+	DIAGNOSTIC_ring(ring);
+	KASSERT(n >= 0);
+	KASSERTMSG(ring->used + n <= ring->capacity,
+		"%s: ring->used=%d n=%d ring->capacity=%d",
+		__func__, ring->used, n, ring->capacity);
+
+	ring->used += n;
+}
+
+// ring->head の位置からの有効フレームにアクセスしようとするとき、
+// ラウンディングせずにアクセス出来る個数を返します。
+/*
+ * Return the number of contiguous frames in used.
+ */
+static inline int
+audio_ring_get_contig_used(const audio_ring_t *ring)
+{
+	DIAGNOSTIC_ring(ring);
+
+	if (ring->head + ring->used <= ring->capacity) {
+		return ring->used;
+	} else {
+		return ring->capacity - ring->head;
+	}
+}
+
+// audio_ring_tail の位置から空きフレームにアクセスしようとするとき、
+// ラウンディングせずにアクセス出来る、空きフレームの個数を返します。
+/*
+ * Return the number of contiguous free frames.
+ */
+static inline int
+audio_ring_get_contig_free(const audio_ring_t *ring)
+{
+	DIAGNOSTIC_ring(ring);
+
+	// ring の折り返し終端まで使用されているときは、
+	// 開始位置はラウンディング後なので < が条件
+	if (ring->head + ring->used < ring->capacity) {
+		return ring->capacity - (ring->head + ring->used);
+	} else {
+		return ring->capacity - ring->used;
+	}
+}
 
 #endif /* !_SYS_DEV_AUDIO_AUDIODEF_H_ */
