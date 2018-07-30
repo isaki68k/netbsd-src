23e4c488dc8c335a60c86274a6d73d3a33a1c38e
moveccr <moveccr@>
2018-07-30 22:45:11 +0900

    xpcmd はタイムアウト付きで READY を待つようにした。 XPLX 側にファームウェアロードして READY になるまで多少の時間が かかるおそれがある。

--- sys/arch/luna68k/dev/xpcmd.c
+++ sys/arch/luna68k/dev/xpcmd.c
@@ -4,6 +4,7 @@
 #if defined(_KERNEL)
 
 #include <sys/stdint.h>
+#include <machine/param.h>
 
 #include <arch/luna68k/dev/xpbusvar.h>
 #include <arch/luna68k/dev/xpcmd.h>
@@ -21,13 +22,27 @@ const int READY = 0;
 const int CMD = 1;
 const int RESULT = 2;
 
+static
+int
+xp_waitfor_ready(int xplx_devid, int timeout)
+{
+	uint16_t addr = XPLX_VAR_BASE + xplx_devid * 16;
+	do {
+		int r;
+		r = xp_readmem8(addr + READY);
+		if (r) return 1;
+		delay(100);
+	} while (timeout--);
+	return 0;
+}
+
 int
 xp_cmd_nowait(int xplx_devid, uint8_t cmd)
 {
 	uint8_t r;
 
 	uint16_t addr = XPLX_VAR_BASE + xplx_devid * 16;
-	r = xp_readmem8(addr + READY);
+	r = xp_waitfor_ready(xplx_devid, 1000);
 	if (r == 0) return 0;
 	xp_writemem8(addr + RESULT, 0);
 	xp_writemem8(addr + CMD, cmd);
@@ -41,7 +56,7 @@ xp_cmd(int xplx_devid, uint8_t cmd)
 	uint8_t r;
 
 	uint16_t addr = XPLX_VAR_BASE + xplx_devid * 16;
-	r = xp_readmem8(addr + READY);
+	r = xp_waitfor_ready(xplx_devid, 1000);
 	if (r == 0) return 0;
 	xp_writemem8(addr + RESULT, 0);
 	xp_writemem8(addr + CMD, cmd);
