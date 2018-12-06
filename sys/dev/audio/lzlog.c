9de082841bf691eb4624b6f6aacf1ea82cd6c0a3
Tetsuya Isaki <isaki@pastel-flower.jp>
2018-12-06 19:39:50 +0900

    lzlog をいろいろ修正。

--- sys/dev/audio/lzlog.c
+++ sys/dev/audio/lzlog.c
@@ -18,7 +18,7 @@ static void lzlog_flush(void);
 
 static char *lzlog_buf;
 static int lzlog_size;
-static int lzlog_cur;	// 次の書き込み位置 (現在の末尾 '\0' の位置)
+static int lzlog_wptr;	// 次の書き込み位置 (現在の末尾 '\0' の位置)
 
 // サイズは確保するメモリサイズ。
 static void
@@ -27,15 +27,13 @@ lzlog_open(int size)
 	lzlog_size = size;
 	lzlog_buf = kmem_zalloc(lzlog_size, KM_SLEEP);
 
-	// 最後に番兵を入れる
-	lzlog_size--;
-	lzlog_cur = 0;
+	lzlog_wptr = 0;
 }
 
 static void
 lzlog_close(void)
 {
-	kmem_free(lzlog_buf, lzlog_size + 1);
+	kmem_free(lzlog_buf, lzlog_size);
 	lzlog_buf = NULL;
 }
 
@@ -43,51 +41,55 @@ static void
 lzlog_printf(const char *fmt, ...)
 {
 	char buf[1024];
-	int len;
 	va_list ap;
+	int i;
 
 	va_start(ap, fmt);
-	len = vsnprintf(buf, sizeof(buf), fmt, ap);
+	vsnprintf(buf, sizeof(buf), fmt, ap);
 	va_end(ap);
 
 	// メモリ確保されてなければフォールバック
 	if (lzlog_buf == NULL) {
-		printf("**%s", buf);
+		printf("* %s", buf);
 		return;
 	}
 
-	// len に '\0' の長さは入ってないけど書き込むので計算に注意。
-
-	if (lzlog_cur + len < lzlog_size) {
-		strcpy(lzlog_buf + lzlog_cur, buf);
-		lzlog_cur = (lzlog_cur + len) % lzlog_size;
-	} else {
-		// 折り返しが発生する
-		int len1 = lzlog_size - lzlog_cur;
-		int len2 = len - len1;
-		memcpy(lzlog_buf + lzlog_cur, buf, len1);
-		strcpy(lzlog_buf, buf + len1);
-		lzlog_cur = len2;
+	for (i = 0; buf[i] != '\0'; i++) {
+		lzlog_buf[lzlog_wptr] = buf[i];
+		lzlog_wptr = (lzlog_wptr + 1) % lzlog_size;
 	}
+	lzlog_buf[lzlog_wptr] = '\0';
 }
 
 static void
 lzlog_flush(void)
 {
-	// cur は '\0' の位置を指しているので、'\0' である間スキップ
-	while (++lzlog_cur < lzlog_size) {
-		if (lzlog_buf[lzlog_cur] != '\0')
-			break;
+	char buf[1024];
+	int rptr;
+	int i;
+
+	rptr = lzlog_wptr - 1;
+	if (rptr < 0)
+		rptr = lzlog_size - 1;
+
+	// 先頭を探す
+	for (; lzlog_buf[rptr] != '\0'; ) {
+		rptr--;
+		if (rptr < 0)
+			rptr = lzlog_size - 1;
 	}
-	if (lzlog_cur == lzlog_size) {
-		// 末尾まで '\0' だったら、まだ折り返しが発生してない
-		printf("%s", lzlog_buf);
-	} else {
-		// 例えばlzlog_bufが10 バイトのバッファ(9バイト有効)で cur が 6なら
-		//  0 1 2 3 4  5 6 7 8  9
-		// |D E F G H \0 A B C \0<-番兵
-		printf("%s", lzlog_buf + lzlog_cur);
-		printf("%s", lzlog_buf);
+	rptr = (rptr + 1) % lzlog_size;
+
+	i = 0;
+	for (; lzlog_buf[rptr] != '\0'; rptr = (rptr + 1) % lzlog_size) {
+		int ch = lzlog_buf[rptr];
+		buf[i++] = ch;
+
+		if (ch == '\n') {
+			buf[i] = '\0';
+			printf("%s", buf);
+			i = 0;
+		}
 	}
 }
 
@@ -97,7 +99,7 @@ main(int ac, char *av[])
 {
 	int i;
 
-	lzlog_open(8);
+	lzlog_open(10);
 	for (i = 1; i < ac; i++) {
 		lzlog_printf("%s\n", av[i]);
 	}
