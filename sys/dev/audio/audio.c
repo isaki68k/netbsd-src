94acc3617e532991901531ff8df9f4a2f5e5feb9
Tetsuya Isaki <isaki@pastel-flower.jp>
2018-12-10 14:58:34 +0900

    sc_dying を立てたら broadcast しないといけない気がする。

--- sys/dev/audio/audio.c
+++ sys/dev/audio/audio.c
@@ -1190,6 +1190,7 @@ audioactivate(device_t self, enum devact act)
 	case DVACT_DEACTIVATE:
 		mutex_enter(sc->sc_lock);
 		sc->sc_dying = true;
+		cv_broadcast(&sc->sc_exlockcv);
 		mutex_exit(sc->sc_lock);
 		return 0;
 	default:
@@ -1220,6 +1221,7 @@ audiodetach(device_t self, int flags)
 	// XXX ここで file を解放しないといけないのでは?
 	mutex_enter(sc->sc_lock);
 	sc->sc_dying = true;
+	cv_broadcast(&sc->sc_exlockcv);
 	SLIST_FOREACH(f, &sc->sc_files, entry) {
 		if (f->ptrack)
 			cv_broadcast(&f->ptrack->outchan);
