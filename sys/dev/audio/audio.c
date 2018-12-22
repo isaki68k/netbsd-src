3dc2e4a56ebc07d3f5e78126c63286ed95b6d3f3
Tetsuya Isaki <isaki@pastel-flower.jp>
2018-12-22 19:14:11 +0900

    sc_lock, sc_intr_lock はこっちではなく MD ドライバ側で解放してある。

--- sys/dev/audio/audio.c
+++ sys/dev/audio/audio.c
@@ -1296,9 +1296,6 @@ audiodetach(device_t self, int flags)
 
 	cv_destroy(&sc->sc_exlockcv);
 
-	mutex_destroy(sc->sc_intr_lock);
-	mutex_destroy(sc->sc_lock);
-
 #if defined(AUDIO_DEBUG_MLOG)
 	audio_mlog_free();
 #endif
