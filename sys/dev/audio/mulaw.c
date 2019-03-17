5f1f6ff9a737758f52d7432e101e96b28c13cf62
Tetsuya Isaki <isaki@pastel-flower.jp>
2019-03-17 20:59:10 +0900

    comment.

--- sys/dev/audio/mulaw.c
+++ sys/dev/audio/mulaw.c
@@ -74,7 +74,8 @@ __KERNEL_RCSID(0, "$NetBSD$");
  *
  * 2. Calculation (default)
  *  It calculates mu-law with full spec and its precision is 14bit.
- *  It's about 10 times slower but the size is less than a half (on m68k).
+ *  It's about 10 times slower but the size is less than a half (on m68k,
+ *  for example).
  *
  * mu-law is no longer a popular format.  I think size-optimized is better.
  */
