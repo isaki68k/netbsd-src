10d6dc8acd5e9c47cc007eeb46161278065f2625
Tetsuya Isaki <isaki@pastel-flower.jp>
2019-05-04 12:19:56 +0900

    mavb(4): query_formats の返す precision 間違えてたので修正。

--- sys/arch/sgimips/mace/mavb.c
+++ sys/arch/sgimips/mace/mavb.c
@@ -111,8 +111,8 @@ static const struct audio_format mavb_formats[] = {
 	{
 		.mode		= AUMODE_PLAY,
 		.encoding	= AUDIO_ENCODING_SLINEAR_BE,
-		.validbits	= 16,
-		.precision	= 16,
+		.validbits	= 24,
+		.precision	= 32,
 		.channels	= 2,
 		.channel_mask	= AUFMT_STEREO,
 		.frequency_type	= 0,
