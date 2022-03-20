d7502dc459b0ee837c1a8e93daa0ca2380ad55f8
isaki68k <isaki@pastel-flower.jp>
2022-03-20 13:11:00 +0900

    audio_track_record() の呼び出し条件を整理する。 audio_track_record() は usrbuf が1ブロック空いてることを期待しているが これを呼び出す audio_record() がそれをチェックしていないように見える。 トラック内で変換がない時(録音フォーマットが HW フォーマットと同じ時)は トラック内の input buf == outbuf でこれが 3ブロックになるはずで、 usrbuf の空きが1ブロック未満の時に audio_track_record() を呼び出すと audio_track_record() は書き込める分だけ usrbuf に転送するため、 outbuf はブロック単位でなくなる。これにより outbuf が1ブロック未満に なれば audio_rmixer_process() が新着データを track に書き込む際に 半端ブロックをクリーンにしてくれるので、トラック内変換があれば outbuf は 1ブロック長しかないので常にこれが成立して問題ないが、 トラック内変換がない場合 outbuf が1ブロック未満でない半端が発生する。 それが PR/56644 の panic になるはず。

--- sys/dev/audio/audio.c
+++ sys/dev/audio/audio.c
@@ -2804,7 +2804,12 @@ audio_read(struct audio_softc *sc, struct uio *uio, int ioflag,
 		mutex_exit(sc->sc_lock);
 
 		audio_track_lock_enter(track);
-		audio_track_record(track);
+		/* Convert as many blocks as possible. */
+		while (usrbuf->used <=
+		            track->usrbuf_usedhigh - track->usrbuf_blksize &&
+		    input->used > 0) {
+			audio_track_record(track);
+		}
 
 		/* uiomove from usrbuf as much as possible. */
 		bytes = uimin(usrbuf->used, uio->uio_resid);
@@ -4979,6 +4984,8 @@ audio_track_record(audio_track_t *track)
 	/* Copy outbuf to usrbuf */
 	outbuf = &track->outbuf;
 	usrbuf = &track->usrbuf;
+	/* usrbuf must have at least one free block. */
+	KASSERT(usrbuf->used <= track->usrbuf_usedhigh - track->usrbuf_blksize);
 	/*
 	 * framesize is always 1 byte or more since all formats supported
 	 * as usrfmt(=output) have 8bit or more stride.
@@ -4990,8 +4997,7 @@ audio_track_record(audio_track_t *track)
 	 * bytes is the number of bytes to copy to usrbuf.
 	 */
 	count = outbuf->used;
-	count = uimin(count,
-	    (track->usrbuf_usedhigh - usrbuf->used) / framesize);
+	count = uimin(count, track->usrbuf_blksize / framesize);
 	bytes = count * framesize;
 	if (auring_tail(usrbuf) + bytes < usrbuf->capacity) {
 		memcpy((uint8_t *)usrbuf->mem + auring_tail(usrbuf),
