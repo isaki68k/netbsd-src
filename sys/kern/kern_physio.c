8b71783f80d6a059c20fa2bab68923d0fb0ad03a
isaki68k <isaki@pastel-flower.jp>
2022-08-04 11:27:22 +0900

    catch up b5ec2394dc 2022-08-03 on iij-netbsd-src.

--- sys/kern/kern_physio.c
+++ sys/kern/kern_physio.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kern_physio.c,v 1.100 2022/03/13 18:37:10 riastradh Exp $	*/
+/*	$NetBSD: kern_physio.c,v 1.102 2022/07/10 23:11:55 riastradh Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1990, 1993
@@ -71,7 +71,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: kern_physio.c,v 1.100 2022/03/13 18:37:10 riastradh Exp $");
+__KERNEL_RCSID(0, "$NetBSD: kern_physio.c,v 1.102 2022/07/10 23:11:55 riastradh Exp $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -180,7 +180,7 @@ physio_biodone(struct buf *bp)
 	KASSERT(bp->b_bcount <= todo);
 	KASSERT(bp->b_resid <= bp->b_bcount);
 	if (done == todo)
-		KASSERT(bp->b_error == 0);
+		KASSERTMSG(bp->b_error == 0, "error=%d", bp->b_error);
 #endif /* defined(DIAGNOSTIC) */
 
 	workqueue_enqueue(physio_workqueue, &bp->b_work, NULL);
@@ -308,7 +308,7 @@ physio(void (*strategy)(struct buf *), struct buf *obp, dev_t dev, int flags,
 				 * aligned, the block addresses are used to track
 				 * errors of finished requests.
 				 */
-				if (dbtob(bp->b_blkno) != uio->uio_offset) {
+				if (uio->uio_offset & (DEV_BSIZE - 1)) {
 					error = EINVAL;
 					goto done;
 				}
--- sys/sys/param.h
+++ sys/sys/param.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: param.h,v 1.710 2022/05/22 21:31:48 riastradh Exp $	*/
+/*	$NetBSD: param.h,v 1.712 2022/07/18 04:30:30 thorpej Exp $	*/
 
 /*-
  * Copyright (c) 1982, 1986, 1989, 1993
@@ -67,7 +67,7 @@
  *	2.99.9		(299000900)
  */
 
-#define	__NetBSD_Version__	999009700	/* NetBSD 9.99.97 */
+#define	__NetBSD_Version__	999009900	/* NetBSD 9.99.99 */
 
 #define __NetBSD_Prereq__(M,m,p) (((((M) * 100000000) + \
     (m) * 1000000) + (p) * 100) <= __NetBSD_Version__)
